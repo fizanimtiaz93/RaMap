e21456bff20b0d6eb2d2dc05b0925bf3daa489ef
Joseph LeRoy <jleroy1@fordham.edu>
2014-12-04 02:42:28 -0500

    First commit.

---
+++ app/src/main/java/com/example/ramap/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ramap;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MainActivity extends FragmentActivity {
+    private GoogleMap mMap;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        setUpMapIfNeeded();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+    }
+
+    private void setUpMapIfNeeded() {
+        if (mMap != null) {
+            return;
+        }
+        mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        if (mMap == null) {
+            return;
+        }
+        // Initialize map options. For example:
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        // Enable the My Location layer on the Map:
+        mMap.setMyLocationEnabled(true);
+    }
+}
