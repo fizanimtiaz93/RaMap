935d5e20615ceb7e92bb323b5f1d8c52f3040522
Joseph LeRoy <jleroy1@fordham.edu>
2014-12-10 11:51:40 -0500

    Added building info

--- app/build/generated/source/r/debug/com/example/ramap/R.java
+++ app/build/generated/source/r/debug/com/example/ramap/R.java
@@ -363,16 +363,17 @@ containing a value of this type.
         public static final int powered_by_google_light=0x7f020022;
     }
     public static final class id {
-        public static final int about_menu=0x7f0a0024;
+        public static final int McGinley_center=0x7f0a001a;
+        public static final int about_menu=0x7f0a0025;
         public static final int book_now=0x7f0a000e;
+        public static final int building_list=0x7f0a0018;
         public static final int buyButton=0x7f0a000a;
         public static final int buy_now=0x7f0a000f;
         public static final int buy_with_google=0x7f0a0010;
-        public static final int checkInButton=0x7f0a001b;
-        public static final int checkInLocation=0x7f0a001c;
-        public static final int check_in_history_menu=0x7f0a0022;
+        public static final int checkInButton=0x7f0a001c;
+        public static final int checkInLocation=0x7f0a001d;
+        public static final int check_in_history_menu=0x7f0a0023;
         public static final int classic=0x7f0a0011;
-        public static final int country_list=0x7f0a0018;
         public static final int fragmentContainer=0x7f0a0014;
         public static final int gallery_item_imageView=0x7f0a0017;
         public static final int grayscale=0x7f0a0012;
@@ -380,25 +381,25 @@ containing a value of this type.
         public static final int holo_dark=0x7f0a0005;
         public static final int holo_light=0x7f0a0006;
         public static final int hybrid=0x7f0a0000;
-        public static final int info_button=0x7f0a001d;
+        public static final int info_button=0x7f0a001e;
         public static final int label=0x7f0a0015;
-        public static final int map=0x7f0a001a;
+        public static final int map=0x7f0a001b;
         public static final int match_parent=0x7f0a000c;
-        public static final int menu_item_clear=0x7f0a0021;
-        public static final int menu_item_search=0x7f0a0020;
+        public static final int menu_item_clear=0x7f0a0022;
+        public static final int menu_item_search=0x7f0a0021;
         public static final int monochrome=0x7f0a0013;
         public static final int none=0x7f0a0001;
         public static final int normal=0x7f0a0002;
-        public static final int pics_button=0x7f0a001e;
+        public static final int pics_button=0x7f0a001f;
         public static final int production=0x7f0a0007;
         public static final int sandbox=0x7f0a0008;
         public static final int satellite=0x7f0a0003;
         public static final int selectionDetails=0x7f0a000b;
-        public static final int settings_menu=0x7f0a0023;
+        public static final int settings_menu=0x7f0a0024;
         public static final int strict_sandbox=0x7f0a0009;
         public static final int terrain=0x7f0a0004;
         public static final int textView=0x7f0a0019;
-        public static final int twitter_button=0x7f0a001f;
+        public static final int twitter_button=0x7f0a0020;
         public static final int wrap_content=0x7f0a000d;
     }
     public static final class integer {
--- app/src/main/java/com/example/ramap/InfoActivity.java
+++ app/src/main/java/com/example/ramap/InfoActivity.java
@@ -2,15 +2,37 @@ package com.example.ramap;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by Joe's Desktop on 12/8/2014.
  */
 public class InfoActivity extends Activity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.info_activity);
 
+        //--- text view---
+        TextView txtView = (TextView) findViewById(R.id.McGinley_center);
+        final String Label = txtView.getText().toString();
+
+        txtView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),
+                        "You have clicked the Label : " + Label,
+                        Toast.LENGTH_LONG).show();
+            }
+
+        });
+
+
     }
+
 }
